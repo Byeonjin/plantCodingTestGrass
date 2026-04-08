4911d44d5b6c1b0eb808d290e0d2ef1cd5c6fa5e
변진상 <54176384+Byeonjin@users.noreply.github.com>
2026-04-08 21:06:23 +0900

    [level 2] Title: 모음 사전, Time: 0.54 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -0,0 +1,28 @@
+function solution(word) {
+  let answer = 0;
+  const vowels = ["A", "E", "I", "O", "U"];
+  let found = false;
+
+  const dfs = (current) => {
+    if (found) return;
+
+    if (current.length > 0) {
+      answer++;
+
+      if (current === word) {
+        found = true;
+        return;
+      }
+    }
+
+    if (current.length === 5) return;
+
+    for (let i = 0; i < vowels.length; i++) {
+      dfs(current + vowels[i]);
+    }
+  };
+
+  dfs("");
+
+  return answer;
+}
